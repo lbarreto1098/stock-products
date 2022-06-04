acdaa13666e3d53a5c3c0127a33d0232a76c609d
lbarreto1098 <lbarreto1098@hotmail.com>
2022-06-04 01:55:23 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log('Petición recibida')
+    res.send('Hola Mundo :)')
+})
+
+app.listen(4000, () => {
+    console.log('Escuchando en el puerto 4000 corectamente')
+})

238981db572065d45d13c3157a4d6d0d9853d72d
lbarreto1098 <lbarreto1098@hotmail.com>
2022-06-04 02:48:17 -0500

    Se cambia variable PORT

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 
 const app = express()
@@ -7,6 +8,8 @@ app.get('/', (req, res) => {
     res.send('Hola Mundo :)')
 })
 
-app.listen(4000, () => {
-    console.log('Escuchando en el puerto 4000')
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+    console.log(`Escuchando en el puerto ${PORT}`)
 })

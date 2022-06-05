ea7c4f18a769baf673f961fbbcc0ea4ce8dac37f
lbarreto1098 <lbarreto1098@hotmail.com>
2022-06-05 15:23:01 -0500

    Conexión a MongoDB

--- app.js
+++ app.js
@@ -6,22 +6,43 @@ require('dotenv').config()
 const app = express()
 
 mongoose.connect(
-    `mongodb+srv://lbarreto1098:${process.env.MONGO_DB_PASS}@development.tqihehs.mongodb.net/?retryWrites=true&w=majority`
+  `mongodb+srv://lbarreto1098:${process.env.MONGO_DB_PASS}@development.tqihehs.mongodb.net/stock?retryWrites=true&w=majority`
 )
-.then(result => {
+  .then(result => {
     app.listen(PORT, () => {
-        console.log(`Escuchando en el puerto ${PORT}`)
+      console.log(`Escuchando en el puerto ${PORT}`)
     })
-    console.log('Conexión DB exitosa')})
-.catch(e => console.log(e))
+    console.log('Conexión DB exitosa')
+  })
+  .catch(e => console.log(e))
+
+//Recibe un esquema del producto
+const productSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  precio: Number
+},
+  { timestamps: true } //Crea fecha de creación y actualización
+)
+
+//Modelo para interactuar con la colección
+const Product = mongoose.model('Product', productSchema)
 
 app.use(express.json())
 
-app.post('/api/v1/products', (req, res, next) => {
-    console.log('Petición recibida')
-    console.log({body: req.body})
-    res.status(201).json({ok: true})
-    //next() //sale del middleware
+app.post('/api/v1/products', (req, res) => {
+  //console.log('Petición recibida')
+  //console.log({ body: req.body })
+  //Instanciar un nuevo documento de tipo producto
+  const newProduct = new Product(req.body)
+  //Para guardar
+  newProduct.save()
+  //Si se guarda responer:
+    .then(result => {
+      res.status(201).json({ ok: true })
+    })
+    .catch(e => console.log(e))
+
+  //next() //sale del middleware
 })
 
 //middleware que busca rutas

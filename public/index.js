7536d92637b1328c1de2891aeea41d1fada37766
lbarreto1098 <lbarreto1098@hotmail.com>
2022-06-05 20:14:12 -0500

    Cambios

--- public/index.js
+++ public/index.js
@@ -6,19 +6,19 @@ const buttonCrear = document.querySelector('#buttonCrear')
 //console.log({buttonCrear})
 
 buttonCrear.addEventListener('click', (e) => {
-    //console.log({ name: inputName.value, precio: inputPrecio.value })
-    const name = inputName.value
-    const precio = inputPrecio.value
+  //console.log({ name: inputName.value, precio: inputPrecio.value })
+  const name = inputName.value
+  const precio = inputPrecio.value
 
-    //fetch: librería del navegador para hacer peticiones 
-    fetch('/api/v1/products', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            name,
-            precio
-        })
+  //fetch: librería del navegador para hacer peticiones 
+  fetch('/api/v1/products', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name,
+      precio
     })
+  })
 })
